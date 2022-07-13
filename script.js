fa992cd2750c09732e048eec8f6d8c58fb040caa
Kyle Thomas <kylejwthomas@gmail.com>
2022-07-12 15:40:04 -0900

    created enemy constructor

--- script.js
+++ script.js
@@ -21,20 +21,29 @@ document.addEventListener('load',function(){
 
   class Enemy {
     constructor(){
-
+      this.x = 100;
+      this.y = 100;
+      this.width = 100;
+      this.height = 100;
     }
     update(){
+      this.x--;
 
     }
     draw(){
+      ctx.fillRect(this.x, this.y, this.width. this.height);
 
     }
   }
 
-  function animate(){
+  let lastTime = 1;
+  function animate(timeStamp){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+
     // some code
     requestAnimationFrame(animate);
-  }
-
+  };
+  animate(0);
 });
