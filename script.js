e9bfb951b4d6ea78fc14d0787e4c90534f62679c
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-18 10:08:30 +0900

    updated enemy intervals

--- script.js
+++ script.js
@@ -5,27 +5,39 @@ document.addEventListener('DOMContentLoaded',function(){
   canvas.height = 800;
 
   class Game {
-    constructor() {
+    constructor(ctx, width, height){
+      this.ctx = ctx;
+      this.width = width;
+      this.height = height;
       this.enemies = [];
-      this.#addNewEnemy();
-      console.log(this.enemies);
+      this.enemyInterval = 20;
+      this.enemyTimer = 0;
     }
     update(){
+      if (this.enemyTimer > this.enemyInterval){
+        this.#addNewEnemy();
+        this.enemyTimer = 0;
+        console.log(this.enemies);
+      } else {
+        this.enemyTimer++;
+      }
       this.enemies.forEach(object => object.update());
     }
     draw(){
       this.enemies.forEach(object => object.draw());
     }
     #addNewEnemy(){
-      this.enemies.push(new Enemy());
+      this.enemies.push(new Enemy(this));
 
     }
   }
 
   class Enemy {
-    constructor(){
-      this.x = 100;
-      this.y = 100;
+    constructor(game){
+      this.game = game;
+      console.log(this.game);
+      this.x = this.game.width;
+      this.y = Math.random() * this.game.height;
       this.width = 100;
       this.height = 100;
     }
@@ -39,7 +51,7 @@ document.addEventListener('DOMContentLoaded',function(){
     }
   }
 
-  const game = new Game();
+  const game = new Game(ctx, canvas.width, canvas.height);
   let lastTime = 1;
   function animate(timeStamp){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
