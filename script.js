8a5389069a9e5c281cb8f7921d1108cecf9f7f39
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-22 18:36:49 +0900

    refactoring

--- script.js
+++ script.js
@@ -43,14 +43,24 @@ document.addEventListener('DOMContentLoaded',function(){
     constructor(game){
       this.game = game;
       this.markedForDeletion = false;
+      this.frameX = 0;
+      this.maxFrame = 5;
+      this.frameInterval = 100;
+      this.frameTimer = 0;
     }
     update(deltaTime){
       this.x -= this.vx * deltaTime;
       if (this.x < 0 - this.width) this.markedForDeletion = true;
-
+      if (this.frameTimer > this.frameInterval){
+          if(this.frameX < this.maxFrame) this.frameX++;
+          else this.frameX = 0;
+          this.frameTimer = 0;
+      } else {
+          this.frameTimer += deltaTime;
+      }
     }
     draw(ctx){
-      ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+      ctx.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
 
     }
   }
